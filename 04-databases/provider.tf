07ff392446824a2bab43a7d0c887f071dbaf804a
mothukuru saikiran <obscuresai.com>
2024-12-17 14:40:00 +0530

    message

--- 04-databases/provider.tf
+++ 04-databases/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
+      version = "1.10.2" # AWS provider version, not terraform version
     }
   }
 
